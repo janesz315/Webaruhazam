184790ab8cdaf6cc85f660628cc1a32e37123839
janesz315 <borosuveg6@gmail.com>
2023-01-24 09:28:01 +0100

    request.rest 創作

--- wanda.js
+++ wanda.js
@@ -1,9 +1,12 @@
 // console.log("今日は!");
 const express = require('express')
 const app = express()
+let port = 3000
 
-app.get('/', function (req, res) {
+app.get('/products', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(port, ()=>{
+    console.log(`Express 鯖 良い 端子:${port}`)
+})
